28440ff8eda7e5bf225e3e21e7ad265f983b84f1
trinabh01 <117459397+trinabh01@users.noreply.github.com>
2022-12-18 16:41:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var s1,s2,s3
+
+function setup() 
+{
+  createCanvas(400, 400);
+  s1 = new Student("john",12,6);
+  s1.display()
+  s2=new Student("rishi",9,4)
+  s2.display();
+}
+function draw() 
+{
+  background(51);
+ 
+}
+
+
